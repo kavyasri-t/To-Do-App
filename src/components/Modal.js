b46bf7f3daafc98573b34d2f0c63e870b9d8d0df
kavyasri <tulabandukavyasri21@gmal.com>
2021-11-20 00:35:20 +0530

    updated Form

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
+// const Backdrop = () => {
+//   return <div className={styles.backdrop}></div>;
+// };
+// const ModalPort = (props) => {
+//   return (
+//     <div className={styles.modal}>
+//       <p>Warning</p>
+//       <p className={styles.mssg}>{props.mssg}</p>
+//       <button
+//         type="button"
+//         onClick={() => {
+//           props.closeModal(true);
+//         }}
+//         className={styles.button}
+//       >
+//         ok
+//       </button>
+//     </div>
+//   );
+// };
 const Modal = (props) => {
   return (
-    <div>
+    <>
+      {/* {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById("backdropRoot")
+      )}
+
+      {ReactDOM.createPortal(
+        <ModalPort mssg={props.mssg} closeModal={props.closeModal} />,
+        document.getElementById("modalRoot")
+      )} */}
       <div className={styles.backdrop}></div>
       <div className={styles.modal}>
         <p>Warning</p>
@@ -16,7 +47,7 @@ const Modal = (props) => {
           ok
         </button>
       </div>
-    </div>
+    </>
   );
 };
 export default Modal;
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,32 +1,41 @@
 import style from "./TaskForm.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Modal from "./Modal";
+import ReactDOM from "react-dom";
 const TaskForm = (props) => {
-  const [initial, setInput] = useState("");
+  const inputData = useRef();
+  // const [initial, setInput] = useState("");
   const [valid, setValid] = useState(true);
-  function inputHandler(e) {
-    if (e.target.value.length > 0) setValid(true);
-    setInput(e.target.value);
-  }
+  // function inputHandler(e) {
+  //   if (e.target.value.length > 0) setValid(true);
+  //   setInput(e.target.value);
+  // }
   function submitHandler(e) {
     e.preventDefault();
 
-    if (initial.trim().length === 0) {
+    if (inputData.current.value.trim().length === 0) {
       setValid(false);
       return;
     }
     setValid(true);
-    props.getData(initial);
-    setInput("");
+    props.getData(inputData.current.value);
+    inputData.current.value = ""; //clear the input fields
+    // props.getData(initial);
+    // setInput("");
   }
   function modalHandler(data) {
     setValid(data);
   }
   return (
     <>
-      {!valid && (
-        <Modal closeModal={modalHandler} mssg="Please enter the text!"></Modal>
-      )}
+      {!valid &&
+        ReactDOM.createPortal(
+          <Modal
+            closeModal={modalHandler}
+            mssg="Please enter the text!"
+          ></Modal>,
+          document.getElementById("modalRoot")
+        )}
       <form onSubmit={submitHandler}>
         <label>Add Task</label>
         <br></br>
@@ -34,8 +43,8 @@ const TaskForm = (props) => {
           // style={{ borderColor: valid ? "#8aecdc" : "red" }}
           type="text"
           className={`${style.itask} ${valid ? "" : style.invalid}`}
-          value={initial}
-          onChange={inputHandler}
+          // onChange={inputHandler}
+          ref={inputData}
         ></input>
         <br></br>
         <button className={style.button} type="submit">
